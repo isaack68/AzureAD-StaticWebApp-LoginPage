b4318c3a538063f4a848ec6c29b32a21c7a7cd14
isaack68 <147448945+isaack68@users.noreply.github.com>
2023-10-10 15:03:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const signUpBtn = document.getElementById('signIn');
 
 signUpBtn.addEventListener('click', () => {
-    window.location.replace("/.auth/login/aad?post_login_redirect_uri=https://calm-bush-04d27fd10.3.azurestaticapps.net/authenticated/");
+    window.location.replace("/.auth/login/aad?post_login_redirect_uri=https://https://ambitious-pond-0c1d1fa10.3.azurestaticapps.net/authenticated/");
 });
